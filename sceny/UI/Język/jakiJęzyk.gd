1ca46eccea4c458925460789bcf84ea5dd65501d
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-01-30 10:51:30 +0100

    utworzenie systemu sczytywania danych w skrypcie jakiJęzyk.gd
    System sczytuje dane na temat domyślnego języka w grze po czym podaje go jako pierwszą opcję w programie.

--- sceny/UI/Język/jakiJęzyk.gd
+++ sceny/UI/Język/jakiJęzyk.gd
@@ -1,24 +1,42 @@
 extends Label
 
-@onready var ścieżkaDoPliku = "res://sceny/UI/Język/języki.txt"
+@onready var ścieżkaDoPlikuZDostępnymiJęzykami = "res://sceny/UI/Język/języki.txt"
+@onready var ścieżkaDoPlikuZGłównymiDanymi = "res://DaneGry/główneDane.txt"
+var językDomyślny;
 var języki = []
 var liczba: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	załadujPlik(ścieżkaDoPliku)
+	językDomyślny = znajdźJęzykDomyślny(ścieżkaDoPlikuZGłównymiDanymi)
+	załadujPlik(ścieżkaDoPlikuZDostępnymiJęzykami, językDomyślny)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.text = " "+języki[liczba]+" "
 
-func załadujPlik(ścieżka: String):
+func załadujPlik(ścieżka: String, język: String):
 	var plik = FileAccess.open(ścieżka, FileAccess.READ)
-	var index = 1
+	var index=0
 	while not plik.eof_reached():
 		var linijka = plik.get_line()
 		języki.append(linijka)
+		if język == języki[index]:
+			liczba = index
+		else:
+			index += 1
+	plik.close()
+
+func znajdźJęzykDomyślny(ścieżka: String):
+	var plik = FileAccess.open(ścieżka, FileAccess.READ)
+	var tabela = []
+	while not plik.eof_reached():
+		var linijka = plik.get_line()
+		tabela = linijka.split(": ")
+		if tabela[0] == "wybranyJęzyk":
+			plik.close()
+			return tabela[1]
 
 func _on_w_lewo_pressed():
 	if liczba > 0:

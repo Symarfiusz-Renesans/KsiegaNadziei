a9119fa8b5ed360868565974dd9650bbca51b785
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-03-19 07:03:17 +0100

    próby wprowadzenia tłumaczeń do gry

--- sceny/UI/Język/językMenu.gd
+++ sceny/UI/Język/językMenu.gd
@@ -11,6 +11,18 @@ func _on_potwierdź_pressed():
 	var liczba = plik.get("liczba")
 	wybranyJęzyk = możliweJęzyki[liczba]
 	ZarzadzanieDanymi.nadpiszDane("wybranyJęzyk", wybranyJęzyk)
+	match wybranyJęzyk:
+		"Polski": 
+			TranslationServer.set_locale("pl")
+		"English": 
+			TranslationServer.set_locale("en")
+		"Deutsch": 
+			TranslationServer.set_locale("de")
+		"Magyarul": 
+			TranslationServer.set_locale("hu")
+		"Esperanta":
+			TranslationServer.set_locale("eo")
+	print(TranslationServer.get_locale)
 	_on_powróć_pressed()
 
 

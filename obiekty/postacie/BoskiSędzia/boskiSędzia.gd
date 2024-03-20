ff97bc65443081ecbf81ba7ceafa2519c670f610
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-03-20 20:36:04 +0100

    ukończenie dodawania tłumaczeń i naprawienie Pauzy
    Scena Pauza nie pokazywała się, gdy gracz był niewidoczny. Teraz już tak się nie dzieje

--- obiekty/postacie/BoskiSędzia/boskiSędzia.gd
+++ obiekty/postacie/BoskiSędzia/boskiSędzia.gd
@@ -1,5 +1,17 @@
 extends GrywalnaPostać
 
+var czyJestPauza: bool = false
+var czyByłNiewidoczny: bool = false
+
 func _physics_process(delta):
 	if self.visible:
 		kontrolaRuchu(delta)
+	
+	if Input.is_action_just_pressed("pauza"):
+		czyJestPauza = !czyJestPauza
+		if czyJestPauza and !self.visible:
+			self.visible = true
+			czyByłNiewidoczny = true
+		elif czyByłNiewidoczny:
+			self.visible = false
+			czyByłNiewidoczny =false
